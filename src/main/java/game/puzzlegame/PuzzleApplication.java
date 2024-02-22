ff18e6af86ca213841e1345d1a0e8e72a33cd9be
Victor Serra <vserra3192@gmail.com>
2024-02-21 20:06:18 -0500

    Changed to java 1.8

--- src/main/java/game/puzzlegame/PuzzleApplication.java
+++ src/main/java/game/puzzlegame/PuzzleApplication.java
@@ -1,13 +1,9 @@
 package game.puzzlegame;
 
 import javafx.application.Application;
-import javafx.event.Event;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class PuzzleApplication extends Application {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module game.puzzlegame {
     requires javafx.controls;
     requires javafx.fxml;
+    requires rt;
 
 
     opens game.puzzlegame to javafx.fxml;
